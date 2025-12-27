c86f44fb1257b58836334a7eff4cccfb7d9e5e71
dffanzm <milesyourbae@gmail.com>
2025-12-28 05:45:12 +0700

    debug again

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const cors = require("cors");
 const dotenv = require("dotenv");
+const fileUpload = require("express-fileupload"); // <--- 1. Import ini
 
 // Import Routes
 const productRoutes = require("./routes/productRoutes");
@@ -9,18 +10,27 @@ const bannerRoutes = require("./routes/bannerRoutes");
 dotenv.config();
 const app = express();
 
+// Middleware
 app.use(cors());
 app.use(express.json());
+
+// <--- 2. Pasang ini supaya bisa baca req.files di Controller
+app.use(fileUpload());
 
 // Routes
 app.use("/api/products", productRoutes);
 app.use("/api/banners", bannerRoutes);
 
-const PORT = process.env.PORT || 3000;
-app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
+// Root Endpoint (Optional: buat ngecek server nyala)
+app.get("/", (req, res) => {
+  res.send("Bosh Parfume Server is Running!");
+});
 
-//vercel setup
-module.exports = app;
+// Setup Port & Vercel
+// Logic ini biar aman: Kalau di Vercel dia export 'app', kalau di local dia 'listen'
 if (require.main === module) {
-  app.listen(3000, () => console.log("Server ready on port 3000."));
+  const PORT = process.env.PORT || 3000;
+  app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
 }
+
+module.exports = app;
